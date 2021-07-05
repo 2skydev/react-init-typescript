4b3ff909b0c28215ba2778471d0ec42d9c2cf5c7
KDW <42798237+dawan0111@users.noreply.github.com>
2021-07-05 15:20:06 +0900

    prettier import 정렬 추가

--- packages/frontend/client/src/App.tsx
+++ packages/frontend/client/src/App.tsx
@@ -1,17 +1,25 @@
-import Abc from './Abc'
-import { SWRConfig } from 'swr'
-import axios from 'shared/apis'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
+import _ from 'lodash';
+import { SWRConfig } from 'swr';
+
+import axios from 'shared/apis';
+
+import Abc from './Abc';
 
 function App() {
-  return (
-    <SWRConfig value={{
-      revalidateOnFocus: false,
-      fetcher: url => axios.get(url)
-    }}>
-      <Abc />
-    </SWRConfig>
-  )
+    return (
+        <SWRConfig
+            value={{
+                revalidateOnFocus: false,
+                fetcher: url => axios.get(url),
+            }}
+        >
+            <BrowserRouter></BrowserRouter>
+            <Abc />
+        </SWRConfig>
+    );
 }
 
-export default App
+export default App;
